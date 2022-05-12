1f2b3219377c807a41981f5a008cb1670f1581a5
Alejandra Diaz <francys.diazurribarri2@gmail.com>
2022-05-12 12:40:16 +0200

    adds expres.js and cors

---
+++ src/index.js
@@ -0,0 +1,21 @@
+// Fichero src/index.js
+
+// Importamos los dos módulos de NPM necesarios para trabajar
+const express = require('express');
+const cors = require('cors');
+
+// Creamos el servidor
+const server = express();
+
+// Configuramos el servidor
+server.use(cors());
+server.use(express.json());
+
+// Arrancamos el servidor en el puerto 4000
+const serverPort = 4000;
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// Escribimos los endpoints que queramos
+server.get('/card', (req, res) => {});
